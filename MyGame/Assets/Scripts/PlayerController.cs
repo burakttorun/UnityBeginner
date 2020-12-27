c19abf12ed14eaa68f52902a06b9d2bb8bf1c71c
Burak Torun <burakttorun@hotmail.com>
2020-12-27 22:35:59 +0300

    Challenge3 added

---
+++ MyGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed=10f;
+    private Rigidbody playerRB;
+
+    private float zBound = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+
+        ConstrainPlayerPosition();
+    }
+
+    private void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        else if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRB.AddForce(Vector3.forward * verticalInput * speed);
+        playerRB.AddForce(Vector3.right * horizontalInput * speed);
+    }
+}

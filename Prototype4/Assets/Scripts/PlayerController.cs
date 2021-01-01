0042bcf46c1f3f4d2cfbb1c9c5652263e0ee46af
Burak Torun <burakttorun@hotmail.com>
2021-01-01 23:19:39 +0300

    Prototype4 is created

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody playerRB;
+    private GameObject focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = gameObject.GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}

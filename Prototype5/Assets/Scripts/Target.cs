7d0b6789bfd6bb61377a799b3894c6645fd08833
Burak Torun <burakttorun@hotmail.com>
2021-01-05 21:30:30 +0300

    Clicky Mouse part added to Prototype5

--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody targetRb;
+
+    private float minSpeed = 12f, maxSpeed = 16f;
+    private float maxTorque = 10f;
+    private float xRange = 4f, ySpawnPos = -2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+        transform.position = RandomSpawnPos();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }

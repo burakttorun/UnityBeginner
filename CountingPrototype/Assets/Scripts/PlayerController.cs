6185f4d6f66251100e4dbd8a72a77948b593f85a
Burak Torun <burakttorun@hotmail.com>
2021-01-24 23:39:08 +0300

    CountingPrototype screen design was done.

---
+++ CountingPrototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{ 
+    [SerializeField]
+    float moveSpeed;
+
+    float moveAxisX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveAxisX = Input.GetAxis("Horizontal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * moveAxisX * moveSpeed * Time.deltaTime);
+    }
+}
